dc1d6e9d69e9fab0e86ad6231f4c4fefb7875c4b
Angelino Valeta <angelinovaleta@gmail.com>
2023-10-14 13:53:00 +0100

    Use case Create Product Web

---
+++ internal/infra/web/product_handlers.go
@@ -0,0 +1,39 @@
+package web
+
+import (
+  "encoding/json"
+  "net/http"
+  "github.com/angelino-valeta/hexagonal-akakfa-golang-crud-product/internal/usecase"
+)
+
+type ProductHandlers struct {
+  CreateProductUseCase *usecase.CreateProductUseCase
+  ListProductUseCase *usecase.ListProductUseCase
+}
+
+func NewProductHandlers(createProductUseCase *usecase.CreateProductUseCase, listProductUseCase *usecase.ListProductUseCase) *ProductHandlers {
+  return &ProductHandlers {
+    CreateProductUseCase: createProductUseCase,
+    ListProductUseCase: listProductUseCase
+  }
+}
+
+func (p *ProductHandlers) CreateProductHandler(w http.ResponseWriter, r *http.Request){
+  var input usecase.CreateProductInputDto
+  err := json.newDecoder(r.Body).Decode(&input)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+  output, err := p.CreateUseCase.Execute(input)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusCreated)
+  json.NewEnconder(w).Enconde(output)
+}
+
+func () LstProductHandler(){}
